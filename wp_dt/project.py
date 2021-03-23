e483f5e88fc89fab5787f21ed3fffb10e3e28537
dev <dev@qiyutech.tech>
2021-03-23 12:21:25 +0800

    allow none

--- wp_dt/project.py
+++ wp_dt/project.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 from .wp import WPReadMe
@@ -8,5 +10,5 @@ __all__ = ["WPProject"]
 class WPProject(BaseModel):
     name: str = Field(..., title="项目名称")
     revision: int = Field(..., title="当前版本")
-    date: str = Field(..., title="更新时间")
+    date: Optional[str] = Field(None, title="更新时间")
     readme: WPReadMe = Field(..., title="ReadMe")
